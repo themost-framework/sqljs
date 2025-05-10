6e53c5b10ef8c3f398d0b958874d5ac33b1237c8
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2025-05-10 17:42:12 +0300

    update test

--- spec/LocalSqlAdapter.spec.ts
+++ spec/LocalSqlAdapter.spec.ts
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom/jest-globals';
 import { expect } from '@jest/globals';
-import {LocalSqlAdapter} from "@themost/sqljs";
+import { LocalSqlAdapter } from "@themost/sqljs";
 
-describe('sql.js', () => {
+describe('LocalSqlAdapter', () => {
     it('should create instance', () => {
         expect(new LocalSqlAdapter()).toBeInstanceOf(LocalSqlAdapter);
     });
@@ -12,11 +12,14 @@ describe('sql.js', () => {
         let exists = await db.table('test').existsAsync();
         expect(exists).toBe(false);
         await db.table('test').createAsync([
-            {name: 'id', type: 'Counter', primary: true},
-            {name: 'name', type: 'Text'},
-            {name: 'age', type: 'Integer'}
+            { name: 'id', type: 'Counter', primary: true },
+            { name: 'name', type: 'Text' },
+            { name: 'age', type: 'Integer' }
         ]);
         exists = await db.table('test').existsAsync();
+        expect(exists).toBe(true);
+        const db2 = new LocalSqlAdapter();
+        exists = await db2.table('test').existsAsync();
         expect(exists).toBe(true);
     });
 });
